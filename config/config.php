b25cc3a757b37095f644acdd1dcc217c5ae64b9c
Zsolt148 <b.zsolt148@gmail.com>
2022-11-11 21:06:19 +0100

    testing

--- config/config.php
+++ config/config.php
@@ -4,6 +4,28 @@
 //Site name
 define('SITE_NAME', 'Notebooks-php');
 
+$documentRoot = null;
+
+if (isset($_SERVER['DOCUMENT_ROOT'])) {
+	$documentRoot = $_SERVER['DOCUMENT_ROOT'];
+
+	if (strstr($documentRoot, '/') || strstr($documentRoot, '\\')) {
+		if (strstr($documentRoot, '/')) {
+			$documentRoot = str_replace('/', DIRECTORY_SEPARATOR, $documentRoot);
+		}
+		elseif (strstr($documentRoot, '\\')) {
+			$documentRoot = str_replace('\\', DIRECTORY_SEPARATOR, $documentRoot);
+		}
+	}
+
+	if (preg_match('/[^\\/]{1}\\[^\\/]{1}/', $documentRoot)) {
+		$documentRoot = preg_replace('/([^\\/]{1})\\([^\\/]{1})/', '\\1DIR_SEP\\2', $documentRoot);
+		$documentRoot = str_replace('DIR_SEP', '\\\\', $documentRoot);
+	}
+}
+dd($documentRoot);
+
+define('SERVER_DOC_ROOT', $documentRoot);
 //App Root
 define('APP_ROOT', dirname(dirname(__FILE__)));
 define('URL_ROOT', '/');
